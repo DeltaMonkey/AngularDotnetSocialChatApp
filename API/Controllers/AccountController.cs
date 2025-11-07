7e80d051dd5c65079de12be8d5972bc2d741fd7f
DeltaMonkey <engincanozkan@outlook.com>
2025-11-08 00:57:25 +0300

    basic login method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,6 +29,25 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await dbContext.AppUsers.SingleOrDefaultAsync(q => q.Email.ToLower() == loginDto.Email.ToLower());
+
+            if (user == null) return Unauthorized("Invalid email or password.");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid email or password.");
+            }
+
+            return user;
+        }
+
         private async Task<bool> EmailExists(string email)
         {
             return await dbContext.AppUsers.AsNoTracking().AnyAsync(q => q.Email.ToLower() == email.ToLower());
